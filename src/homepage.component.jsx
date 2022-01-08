2d2dd0705e7883104c062c92f00861c43fe6dd36
Christian Sheen <csheen1725@gmail.com>
2022-01-07 21:24:11 -0500

    homepage.component.jsx update

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <div classNam="menu-item">
+        <div className="content">
+          <div className="title">HATS</div>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div classNam="menu-item">
+        <div className="content">
+          <div className="title">JACKETS</div>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div classNam="menu-item">
+        <div className="content">
+          <div className="title">SNEAKERS</div>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div classNam="menu-item">
+        <div className="content">
+          <div className="title">WOMENS</div>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+      <div classNam="menu-item">
+        <div className="content">
+          <div className="title">MENS</div>
+          <span className="sub-title">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
